d003c6a4e0aaeda865aa3b5a1140a6d93d31fb87
Emre <emre.tutuncu@hotmail.com.tr>
2022-10-11 18:34:46 +0300

    Blazored.Typeahead  Dropdown added.

---
+++ Shared/ServerResponseModels/BaseResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Inspection_of_Measuring_Instruments.Shared.ServerResponseModels;
+
+public class BaseResponse
+{
+    public bool Success { get; set; } = false;
+    public string ErrorMessage { get; set; } = string.Empty;
+}
---
+++ Shared/ServerResponseModels/ServerResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Inspection_of_Measuring_Instruments.Shared.ServerResponseModels;
+
+public class ServerResponse<T> : BaseResponse
+{
+    public T? Value { get; set; }
+}
